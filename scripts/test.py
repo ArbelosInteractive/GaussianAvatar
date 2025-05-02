3c9e3dfbd8c780539490b4a2e1c485108a5eecc9
sethclim <sethclim@gmail.com>
2025-05-02 13:39:44 -0400

    torch cuda test script

--- scripts/test.py
+++
@@ -1,7 +0,0 @@
-from OpenGL.GL import *
-from OpenGL.GLUT import *
-
-glutInit()
-glutCreateWindow(b"Check GL Version".encode('utf-8'))
-print("OpenGL version:", glGetString(GL_VERSION).decode())
-print("GLSL version:", glGetString(GL_SHADING_LANGUAGE_VERSION).decode())
